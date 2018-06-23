b84c7450cd3a9460c44f793db6ebd64b66829e03
mbyk <marubayashi@gmail.com>
2018-06-23 11:26:33 +0900

    add file upload for slack

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,9 +1,58 @@
+require 'net/http/post/multipart'
+
 class UploadsController < ApplicationController
+
   def create
     photo = params[:upload][:photo]
     slack_channel_id = params[:upload][:slack_channel_id]
+    content_type = photo.content_type
+    original_filename = photo.original_filename
+    tempfile = photo.tempfile
 
-    flash[:success] = "画像をアップロードしました。"
-    redirect_to root_url
+    if send_request(tempfile, original_filename, content_type, slack_channel_id)
+      flash[:success] = "画像をアップロードしました。"
+      redirect_to root_url
+    else
+      flash[:error] = "画像をアップロードできませんでした。"
+      redirect_to root_url
+    end
+
   end
+
+  private
+
+    # Request (POST )
+    def send_request(file, filename, content_type, channel_id)
+      puts "requeset url: #{EasySettings.slack.file_upload_url}"
+      uri = URI(EasySettings.slack.file_upload_url)
+
+      # Create client
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+
+      body = {
+        "token" => User.slack_token_decrypt(current_user.slack_access_token),
+        "channels" => channel_id,
+        "file" => UploadIO.new(file, content_type, filename)
+      }
+    
+      # Create Request
+      req =  Net::HTTP::Post::Multipart.new(uri, body)
+
+      # Add headers
+      req.add_field "Content-Type", "multipart/form-data; charset=utf-8; boundary=__X_PAW_BOUNDARY__"
+    
+      # Fetch Request
+      res = http.request(req)
+      puts "Response HTTP Status Code: #{res.code}"
+      puts "Response HTTP Response Body: #{res.body}"
+      json = JSON.parse(res.body)
+      return json['ok'] == true
+
+     rescue StandardError => e
+       puts "HTTP Request failed (#{e.message})"
+       return false
+     end
+
 end
