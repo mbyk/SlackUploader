5c9b37669846e4c4676aabc1b8e2041bf68e00cc
mbyk <marubayashi@gmail.com>
2018-07-04 15:58:22 +0900

    delete vendor/bundle data

--- vendor/bundle/ruby/2.3.0/gems/easy_settings-0.1.2/lib/easy_settings/rails.rb
+++
@@ -1,8 +0,0 @@
-class EasySettings
-  class Railtie < ::Rails::Railtie
-    initializer "easy_settings" do |app|
-      EasySettings.namespace   ||= Rails.env
-      EasySettings.source_file ||= Rails.root.join("config/settings.yml").to_s
-    end
-  end
-end
--- vendor/bundle/ruby/2.3.0/gems/easy_settings-0.1.2/lib/easy_settings_version.rb
+++
@@ -1,3 +0,0 @@
-module EasySettingsVersion
-  VERSION = "0.1.2"
-end
