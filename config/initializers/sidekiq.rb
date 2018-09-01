3a48ec04ef99b16677e939cb37b5485788784ed9
mbyk <marubayashi@gmail.com>
2018-09-01 17:48:21 +0900

    - change redis port - add docoker-compose.yml for redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 Sidekiq.configure_server do |config|
-	config.redis = { url: 'redis://localhost:6379', namespace: 'upload_sidekiq' }
+	config.redis = { url: 'redis://localhost:6380', namespace: 'upload_sidekiq' }
 end
 
 Sidekiq.configure_client do |config|
-	config.redis = { url: 'redis://localhost:6379', namespace: 'upload_sidekiq'  }
+	config.redis = { url: 'redis://localhost:6380', namespace: 'upload_sidekiq'  }
 end
 
 
